8739e4dc37b622870c36fa8c527466f81382c752
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-10-31 21:35:52 +0100

    api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.api.routers import users
+from app.api.routers import users, auth, photos, tags, comments, ratings
+
 from app.core.config import settings
 from app.db.session import engine
 from app.db.base import Base
@@ -7,11 +8,17 @@ from app.db.base import Base
 # Створюємо всі таблиці (для dev)
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="My FastAPI Project")
+app = FastAPI(title="PhotoShare API")
 
-# Регіструємо роутери
-app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, PhotoShare API!"}
+
+# 🔗 Підключення роутерів
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(photos.router, prefix="/photos", tags=["Photos"])
+app.include_router(tags.router, prefix="/tags", tags=["Tags"])
+app.include_router(comments.router, prefix="/comments", tags=["Comments"])
+app.include_router(ratings.router, prefix="/ratings", tags=["Ratings"])
