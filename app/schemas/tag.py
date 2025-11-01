6ce5f53bf7e1d9bb61e71aa56dc75eb227f22771
Olena Yakovchuk <elenaygolkova1989@gmail.com>
2025-11-01 17:39:14 +0100

    add schemas photo & tag

---
+++ app/schemas/tag.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class TagBase(BaseModel):
+    name: str
+
+class TagCreate(TagBase):
+    pass  
+
+
+class TagOut(TagBase):
+    id: int
+
+    class Config:
+        orm_mode = True
