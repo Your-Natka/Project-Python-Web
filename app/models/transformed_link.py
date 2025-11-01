4e5bfecb9012d95a51e4e954d1c1692dfd51fa34
Olena Yakovchuk <elenaygolkova1989@gmail.com>
2025-11-01 15:56:19 +0100

    add models photo tag transformed_link

---
+++ app/models/transformed_link.py
@@ -0,0 +1,29 @@
+""" Користувачі можуть створювати посилання на трансформоване зображення 
+    для перегляду світлини в вигляді URL та QR-code """
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON, func
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+
+
+class TransformedLink(Base):
+    __tablename__ = "transformed_links"
+
+    id = Column(Integer, primary_key=True, index=True)
+    photo_id = Column(Integer, ForeignKey("photos.id", ondelete="CASCADE"), nullable=False)
+    transformation_params = Column(JSON, nullable=False)  # зберіг. параметри Cloudinary-трансф.
+    url = Column(String, nullable=False)  # для вже трансф. зображ.
+    qr_code_url = Column(String, nullable=False)
+    created_by = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # Зв’язки
+    photo = relationship("Photo", back_populates="transformed_links")
+    creator = relationship("User") # ? треба?
+
+    def __repr__(self): # -> об'єкт 
+        return f"TransformedLink(photo_id={self.photo_id}, url={self.url})"
+    
+    
